baa770cb1353e77d79924e3a4cfa764335918c89
negrahodzic <negra.hodzic@gmail.com>
2020-04-28 20:14:54 +0200

    Dodat Ionic Napravljena komponenta card Generisana default komponenta Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import {CardComponent} from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
