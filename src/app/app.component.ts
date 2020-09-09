3204402b7b669b0c8a242f99e6da0fc0179b40ab
Katarina <56521139+katarina161@users.noreply.github.com>
2020-09-09 14:26:21 +0200

    Odradjen dizajn stranice, brisanje i edit kartice.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,49 +9,9 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // cardGroupClicked: CardGroup;
-  // cardListFilter: Card[];
-  // cardGroupList: CardGroup[];
-  // cardList: Card[];
-  title = 'PomocPriUcenju';
+  title = 'Pomoc pri ucenju';
   display = true;
-  // display2 = false;
 
-  constructor(private router: Router) {
-    // this.cardGroupList = [
-    //   new CardGroup('Matematika'),
-    //   new CardGroup('Biologija'),
-    //   new CardGroup('Fizika'),
-    //   new CardGroup('Hemija'),
-    // ];
-    //
-    // this.cardList = [
-    //   new Card('pitanje 1', 'odgovor 1', 'Matematika'),
-    //   new Card('pitanje 2', 'odgovor 2', 'Matematika'),
-    //   new Card('pitanje 3', 'odgovor 3', 'Hemija'),
-    //   new Card('pitanje 4', 'odgovor 4', 'Hemija')
-    // ];
-    //
-    // this.cardListFilter = [];
+  constructor() {
   }
-
-  displayEventHandler($event: any) {
-    this.display = $event;
-  }
-
-  // groupWasSelected(group: CardGroup): void {
-  //   console.log('Group clicked: ', group.title);
-  //   this.cardGroupClicked = group;
-  //   this.cardList.forEach(value => {
-  //       if (value.groupName === this.cardGroupClicked.title) {
-  //         this.cardListFilter.push(value);
-  //         console.log(value.groupName);
-  //       }
-  //   });
-  //   console.log('Clicked: ', this.cardGroupClicked.title);
-  //
-  // }
-  // displayEventHandler2($event: any) {
-  //   this.display2 = !$event;
-  // }
 }
--- src/app/card-group/card-group.component.ts
+++ src/app/card-group/card-group.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {newArray} from '@angular/compiler/src/util';
 import {Card} from '../models/card';
 import {Subscription} from 'rxjs';
-import {CardGroupServiceService} from '../card-group-service/card-group-service.service';
+import {CardGroupServiceService} from '../services/card-group-service.service';
 
 @Component({
   selector: 'app-card-group',
@@ -12,29 +12,18 @@ import {CardGroupServiceService} from '../card-group-service/card-group-service.
   styleUrls: ['./card-group.component.css']
 })
 export class CardGroupComponent implements OnInit {
-
-  // service: CardGroupServiceService;
-  title: string;
   public cardGroup: CardGroup;
-  private routeSub: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private service: CardGroupServiceService) {
-    this.routeSub = this.activatedRoute.params.subscribe(params => {
-      this.title = params.cardGroup;
-      console.log('Moj title', this.title );
+    this.activatedRoute.params.subscribe(params => {
+      const title = params.cardGroup;
+      console.log('Moj title', title );
+      this.cardGroup = service.getGroupByName(title);
+      console.log('Servis vraca grupu: ', service.getGroupByName(title));
     });
-    // this.service = new CardGroupServiceService();
-    this.cardGroup = service.getGroupByName(this.title);
-    console.log('Servis vraca grupu: ', service.getGroupByName(this.title));
-    // console.log(this.router.url);
   }
 
   ngOnInit(): void {
-    // this.routeSub = this.activatedRoute.params.subscribe(params => {
-    //   this.title = params.cardGroup;
-    //   this.cardGroup = this.service.getGroupByName(this.title);
-    //   console.log('Moja cardGroup', this.cardGroup );
-    // });
   }
 
 
